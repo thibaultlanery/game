bb17d4170f9454d3cce85bfc4328eaaf416b51e5
Thibault Lanery <thibaultlanery@gmail.com>
2017-07-28 18:57:51 -0500

    badge en cours

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,5 +2,5 @@ class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  enum status: { pending: 0, confirmed: 1, declined: 2 }
+  enum status: { pending: 0, interested: 1, confirmed: 2, declined: 3 }
 end
