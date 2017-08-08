82b0035ff803c39c326a16caa0c5b75e72236523
Thibault Lanery <thibaultlanery@gmail.com>
2017-08-08 18:23:42 -0500

    grid en cours

--- app/assets/javascripts/mCustomScrollbar.js
+++ app/assets/javascripts/mCustomScrollbar.js
@@ -2,7 +2,16 @@
   $(window).on("load",function(){
     $(".mcs-horizontal-example").mCustomScrollbar({
       axis:"x",
-      theme:"dark-3"
+      theme:"dark-3",
+      scrollButtons:{
+        enable: true
+      },
+      scrollButtons:{
+        scrollAmount: auto
+      },
+      keyboard:{
+        enable: true
+      },
     });
   });
 })(jQuery);
