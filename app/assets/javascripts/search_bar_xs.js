383c132a820b8aca461a3efc2b7106fcb315f338
Thibault Lanery <thibaultlanery@gmail.com>
2017-10-30 22:11:15 -0600

    structure ok manque appel de la fonction gmap en JS

--- app/assets/javascripts/search_bar_xs.js
+++ app/assets/javascripts/search_bar_xs.js
@@ -1,11 +1,27 @@
 
-$( ".xs_search_form" ).hide();
+
+$( document ).ready(function() {
+  $( ".xs_search_form" ).hide();
+  // $( ".map-result-layout" ).hide();
+  // $( "#map" ).hide();
+
+    $( ".search_xs_button" ).click(function() {
+      $( ".map-result-layout" ).hide('ease');
+      $( ".xs_search_form" ).toggle('ease');
+
+    });
+    $( ".search_map_xs_button" ).click(function() {
+      $( ".xs_search_form" ).hide('ease');
+       $( ".map-result-layout" ).toggle('ease');
 
 
 
-$( ".search_xs_button" ).click(function() {
-  $( ".xs_search_form" ).toggle('ease');
+    //   // $( "#map" ).toggle('ease');
+
 });
+
+});
+
 
 
 
