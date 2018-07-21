13a15d203cf08ccab4fdb51714127a1d242b1e1b
Thibault Lanery <thibaultlanery@gmail.com>
2018-07-20 20:18:26 -0500

    modif location javascript tag

--- app/controllers/event_type_preferences_controller.rb
+++ app/controllers/event_type_preferences_controller.rb
@@ -2,39 +2,39 @@ class EventTypePreferencesController < ApplicationController
 
 def create
   @user = current_user
-    if params[:id]
-      @event_type_preference = EventTypePreference.new(event_type_params)
-      # @event_type = EventType.new(name: params[:event_type_preference][:event_type_name])
-      # @event_type_preference.event_type = @event_type
-      @event_type_preference.user = @user
-        if @event_type_preference.save
-              respond_to do |format|
-            format.html {   redirect_to user_path(@user) }
-            format.js {} # <-- will render `app/views/reviews/create.js.erb`
-            end
-        else
-              respond_to do |format|
-            format.html { render 'user/show' }
-            format.js {} # <-- idem
-            end
-        end
-    else
-      event_type_ids = params[:event_type_preferences][:event_type_preferences][:event_type]
-      current_user.event_type_preferences << event_type_ids.map { |event_type_id| EventTypePreference.new(event_type_id: event_type_id) }
-      current_user.event_type_preferences.each do |event_type_preference|
-        if event_type_preference.save
+  if params[:id]
+    @event_type_preference = EventTypePreference.new(event_type_params)
+    # @event_type = EventType.new(name: params[:event_type_preference][:event_type_name])
+    # @event_type_preference.event_type = @event_type
+    @event_type_preference.user = @user
+      if @event_type_preference.save
             respond_to do |format|
-          format.html {   redirect_to user_path(@user)}
+          format.html {   redirect_to user_path(@user) }
           format.js {} # <-- will render `app/views/reviews/create.js.erb`
           end
-        else
-             respond_to do |format|
+      else
+            respond_to do |format|
           format.html { render 'user/show' }
           format.js {} # <-- idem
           end
+      end
+  else
+    event_type_ids = params[:event_type_preferences][:event_type_preferences][:event_type]
+    current_user.event_type_preferences << event_type_ids.map { |event_type_id| EventTypePreference.new(event_type_id: event_type_id) }
+    current_user.event_type_preferences.each do |event_type_preference|
+      if event_type_preference.save
+          respond_to do |format|
+        format.html {   redirect_to user_path(@user)}
+        format.js {} # <-- will render `app/views/reviews/create.js.erb`
+        end
+      else
+           respond_to do |format|
+        format.html { render 'user/show' }
+        format.js {} # <-- idem
         end
       end
     end
+  end
 end
 
 
